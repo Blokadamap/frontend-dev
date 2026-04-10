b6e569e52b062fa354e5142452d314d7a3b47f3f
SAMURAI_2035 <sashakedr@icloud.com>
2026-04-10 18:10:52 +0300

    убрал лишнее

--- services/archiveService.ts
+++
@@ -1,7 +0,0 @@
-import { api } from "../lib/api";
-import type { ArchivePayload } from "../types/archive";
-
-export async function getArchiveData() {
-  const response = await api.get<ArchivePayload>("/archive");
-  return response.data;
-}
--- types/map/markers.type.ts
+++
@@ -1,9 +0,0 @@
-import type { Position } from "./coordinates.types"
-
-export type ColorsForMarker = "blue" | "red"
-
-export interface MarkerType {
-    name: string
-    colorIcon: ColorsForMarker
-    position: Position
-}
