80744d99a27773522e3ba1b5bf481422cd76cf86
Vladimir <vkolosov1@mail.ru>
2026-04-11 18:53:41 +0300

    Revert "ui kit"
    This reverts commit 870ddce4790cf9c745ec87a40c985ce0dad906ec.

--- src/components/ui/Input.tsx
+++
@@ -1,8 +0,0 @@
-export const Input = ({ icon, ...props }) => {
-  return (
-    <div className="ui-input-wrapper">
-      {icon && <span>{icon}</span>}
-      <input className="ui-input-field" {...props} />
-    </div>
-  );
-};
